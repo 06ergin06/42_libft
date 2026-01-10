b17d6c79df1dcc4dc3290595fdf1e188aa0226b0
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2026-01-10 23:54:25 +0300

    ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iergin <iergin@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 23:25:26 by iergin            #+#    #+#             */
+/*   Updated: 2026/01/10 23:33:03 by iergin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*m1;
+	unsigned char	*m2;
+	size_t			i;
+
+	m1 = (unsigned char *)s1;
+	m2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (m1[i] != m2[i])
+			return (m1[i] - m2[i]);
+		i++;
+	}
+	return (0);
+}
